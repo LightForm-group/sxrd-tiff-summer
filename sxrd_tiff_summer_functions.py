f5452bc3fe261f23fd2921feac9d086695733eed
Christopher Daniel <christopher.daniel@manchester.ac.uk>
2022-01-11 12:37:25 +0000

    Include subtraction function for subtracting air scattering image
    Include subtraction function for subtracting air scattering image from series of tiff images

--- sxrd_tiff_summer_functions.py
+++ sxrd_tiff_summer_functions.py
@@ -22,9 +22,8 @@ def get_experiment_numbers(start: int, end: int, step: int) -> List[int]:
 
 def extract_input(config_path: str):
     """Extract user inputs from yaml configuration file. 
-    Extract input and output file paths, peak labels, data 
-    resolution and image numbers. These inputs are used
-    to calculate the intensity values from .fit files.
+    Extract input, output and air scattering file paths, 
+    as well as a list of experiment numbers.
     
     :param config_path: path to the configuration file.
     
@@ -43,9 +42,15 @@ def extract_input(config_path: str):
     print("The input path to the series of images is :", input_path, sep = '\n', end = '\n\n')
 
     output_path = config["file_paths"]["output_path"]
-    print("The output path to save the summed/average image is :", output_path, sep = '\n', end = '\n\n')
+    print("The output path to save the adjusted image(s) is :", output_path, sep = '\n', end = '\n\n')
     
-    return experiment_numbers, input_path, output_path
+    air_scatter_path = config["file_paths"]["air_scatter_path"]
+    print("The path to the air scatter image is :", air_scatter_path, sep = '\n', end = '\n\n')
+    
+    air_scatter_multiple = config["file_paths"]["air_scatter_multiple"]
+    print("The value to multiply the air scatter intensity, to match the acquisition frequency of the data, is :", air_scatter_multiple, sep = '\n', end = '\n\n')
+    
+    return experiment_numbers, input_path, output_path, air_scatter_path, air_scatter_multiple
     
 def avg_tiff_images(experiment_number: str, input_filepath: str, output_filepath: str, v_max: int):
     '''Sum up the intensities of all the tiff images contained in the input folder
@@ -57,6 +62,8 @@ def avg_tiff_images(experiment_number: str, input_filepath: str, output_filepath
     :param v_max: intensity maxima for plotting the summed/averaged diffraction pattern image.
     '''
     image_list = sorted(pathlib.Path(input_filepath).glob("0*.tiff"))
+    
+    # set the image arrays to zero to prevent unusual memory allocation
     image_example = io.imread(image_list[1])
     image_array = np.zeros(shape=np.shape(image_example), dtype='int32')
 
@@ -66,7 +73,7 @@ def avg_tiff_images(experiment_number: str, input_filepath: str, output_filepath
     image_array = image_array / len(image_list)
     # convert to integer 32 bit array
     image_array = image_array.astype('int32')
-    plt.imshow(image_array, cmap='gray', vmin = 0, vmax = v_max);
+    plt.imshow(image_array, cmap='gray', vmin = 0, vmax = v_max)
     
     # check if the output directory exists and if not create it
     CHECK_FOLDER = os.path.isdir(f"{output_filepath}")
@@ -98,4 +105,80 @@ def multiple_avg_tiff_images(experiment_numbers: List[int], input_path: str, out
         experiment_number = str(experiment_number)
         input_filepath = input_path.format(experiment_number = experiment_number)
         output_filepath = output_path.format(experiment_number = experiment_number) 
-        avg_tiff_images(experiment_number, input_filepath, output_filepath, v_max)    
+        avg_tiff_images(experiment_number, input_filepath, output_filepath, v_max)
+               
+def subtract_tiff_images(experiment_number: str, air_scatter_filepath: str, air_scatter_multiple: int, input_filepath: str, output_filepath: str, v_max: int):
+    '''Subtract a tiff image (such as an air scattering image) 
+    from each of the tiff images contained in the input folder
+    and save the subtracted tiff images to the output folder.
+    
+    :param experiment_number: input experiment number.
+    :param air_scatter_filepath: path of the tiff image to subtract (such as an air scattering image).
+    :param air_scatter_multiple: value to multiply the air scatter intensity, to match the acquisition frequency of the data.
+    :param input_filepath: input path to the series of tiff images.
+    :param output_filepath: output path to save the series of subtracted tiff images.
+    :param v_max: intensity maxima for plotting the subtracted diffraction pattern image.
+    '''
+    image_list = sorted(pathlib.Path(input_filepath).glob("0*.tif"))
+    number_of_images = len(image_list)
+    
+    # set the image arrays to zero to prevent unusual memory allocation
+    image_example = io.imread(image_list[0])
+    image_array = np.zeros(shape=np.shape(image_example), dtype='int32')
+    air_scatter_image_array = np.zeros(shape=np.shape(image_example), dtype='int32')
+    subtract_image_array = np.zeros(shape=np.shape(image_example), dtype='int32')
+    new_image_array = np.zeros(shape=np.shape(image_example), dtype='int32')
+    
+    air_scatter_image_array = np.array(io.imread(air_scatter_filepath))
+    subtract_image_array = air_scatter_multiple * air_scatter_image_array
+
+    for image_path in tqdm(image_list):
+        
+        image_array = np.array(io.imread(image_path))
+        new_image_array = image_array - subtract_image_array
+
+        # convert to integer 32 bit array
+        new_image_array = new_image_array.astype('int32')
+    
+        # check if the output directory exists and if not create it
+        CHECK_FOLDER = os.path.isdir(f"{output_filepath}")
+
+        if not CHECK_FOLDER:
+            os.makedirs(f"{output_filepath}")
+            print(f"Created folder : '{output_filepath}'.")
+
+        # save the image
+        output_stem = pathlib.Path(image_path).stem
+        io.imsave(f"{output_filepath}{experiment_number}_subtracted_{output_stem}.tif", new_image_array)
+
+    print(f"Written {number_of_images} tiff images to: '{output_filepath}'.", sep = '\n', end = '\n\n')
+    print(f"The AIR SCATTER image, along with the BEFORE / AFTER subtraction images for the final image in the series, are shown below...", sep = '\n', end = '\n\n')
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(subtract_image_array, cmap='gray', vmin = 0, vmax = v_max)
+    plt.subplot(1, 3, 2)
+    plt.imshow(image_array, cmap='gray', vmin = 0, vmax = v_max)
+    plt.subplot(1, 3, 3)
+    plt.imshow(new_image_array, cmap='gray', vmin = 0, vmax = v_max)
+    plt.show()
+    
+def multiple_subtract_tiff_images(experiment_numbers: List[int], air_scatter_filepath: str, air_scatter_multiple: int, input_path: str, output_path: str, v_max: int):
+    '''Create input and output file paths for a list of experiments. 
+    Subtract a tiff image (such as an air scattering image) 
+    from each of the tiff images contained in the input folder
+    and save the subtracted tiff images to the output folder.
+    
+    :param experiment_number: input experiment number.
+    :param air_scatter_filepath: path of the tiff image to subtract (such as an air scattering image).
+    :param air_scatter_multiple: value to multiply the air scatter intensity, to match the acquisition frequency of the data.
+    :param input_filepath: input path to the series of tiff images.
+    :param output_filepath: output path to save the series of subtracted tiff images.
+    :param v_max: intensity maxima for plotting the subtracted diffraction pattern image.
+    '''
+    for experiment_number in experiment_numbers:
+        
+        experiment_number = str(experiment_number)
+        input_filepath = input_path.format(experiment_number = experiment_number)
+        output_filepath = output_path.format(experiment_number = experiment_number)
+        
+        subtract_tiff_images(experiment_number, air_scatter_filepath, air_scatter_multiple, input_filepath, output_filepath,  v_max)
